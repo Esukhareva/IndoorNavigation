30bd435a5afc1f42b0be7ac972fe3980ebd9fd8d
DESKTOP-294T7EA\killk <suharevakati@gmail.com>
2021-03-16 18:35:51 +0300

    работающая версия

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform targetToFollow;    // The transform of the gameobject that gets followed
+    public Quaternion targetRot;        // The rotation of the device camera from Frame.Pose.rotation  
+    public RawImage minimap;            // The rawimage the view of the camera gets rendered to
+    public Camera fullscreenCamera;     // The camera that captures the map (follow camera)
+    public GameObject arrow;            // The direction indicator on the person indicator
+    public GameObject scanButton;       // Button that enables scanning view
+    public GameObject switchButton;     // Button that switches the view
+
+    public float rotationSmoothingSpeed = 1.5f;
+
+    private bool map = false;           // boolean to tell if map is showing (phone position)
+    private bool pressed = false;       // boolean to tell if map is showing (button press)
+    private RenderTexture texture;      // field to save texture to set again after view switch
+
+    // Use lateUpdate to assure that the camera is updated after the target has been updated.
+    void LateUpdate()
+    {
+        if (!targetToFollow) 
+        {
+            return; 
+        }
+
+        Vector3 targetEulerAngles = targetRot.eulerAngles;
+
+        if (targetRot.x > 0.5)
+        {
+            //switch view when phone is turned horizontal
+            if (!map)
+            {
+                //show map
+                map = true;
+                gameObject.GetComponent<Camera>().enabled = false;
+                minimap.gameObject.SetActive(false);
+                texture = fullscreenCamera.targetTexture;
+                fullscreenCamera.targetTexture = null;
+                fullscreenCamera.orthographicSize = 15;
+                scanButton.SetActive(false);
+                switchButton.SetActive(false);
+            }
+        }
+        else 
+        {
+            //show camera view
+            if (map)
+            {
+                map = false;
+                gameObject.GetComponent<Camera>().enabled = true;
+                minimap.gameObject.SetActive(true);
+                fullscreenCamera.targetTexture = texture;
+                fullscreenCamera.orthographicSize = 7;
+                scanButton.SetActive(true);
+                switchButton.SetActive(true);
+            }
+        }
+        // Calculate the current rotation angle around the Y axis we want to apply to the camera.
+        // We add 180 degrees as the device camera points to the negative Z direction
+        float rotationToApplyAroundY = targetEulerAngles.y + 180;
+        // Smooth interpolation between current camera rotation angle and the rotation angle we want to apply.
+        // Use LerpAngle to handle correctly when angles > 360
+        float newCamRotAngleY = Mathf.LerpAngle(arrow.transform.eulerAngles.y, rotationToApplyAroundY, rotationSmoothingSpeed * Time.deltaTime);
+        Quaternion newCamRotYQuat = Quaternion.Euler(0, newCamRotAngleY, 0);
+        if (targetEulerAngles.x < 65) 
+        {
+            arrow.transform.rotation = newCamRotYQuat;
+        }
+
+    }
+
+    //logic when switch button is pressed
+    public void Switch()
+    {
+        if (!pressed)
+        {
+            //show mapview
+            pressed = true;
+            gameObject.GetComponent<Camera>().enabled = false;
+            minimap.gameObject.SetActive(false);
+            texture = fullscreenCamera.targetTexture;
+            fullscreenCamera.targetTexture = null;
+            fullscreenCamera.orthographicSize = 15;
+            scanButton.SetActive(false);
+        }
+        else
+        {
+            //show cameraview
+            pressed = false;
+            gameObject.GetComponent<Camera>().enabled = true;
+            minimap.gameObject.SetActive(true);
+            fullscreenCamera.targetTexture = texture;
+            fullscreenCamera.orthographicSize = 7;
+            scanButton.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/MeasureToolScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeasureToolScript : MonoBehaviour
+{
+    public GameObject arrow_left;
+    public GameObject arrow_right;
+
+    public float arrowScale = 0.15f;
+
+    public float arrowAngle = 0;
+
+    public Color arrowColor;
+
+    public Text textField;
+
+    public float textScale = 0.02f;
+
+    public GameObject canvas;
+
+    float distance;
+
+    private void OnDrawGizmos()
+    {
+        MeasureStuff();
+    }
+
+    void MeasureStuff() 
+    {
+        distance = Vector3.Distance(arrow_left.transform.position, 
+                                    arrow_right.transform.position);
+        textField.text = distance.ToString("N2") + "m";
+
+        canvas.transform.position = LerpByDistance(arrow_left.transform.position, 
+                                                   arrow_right.transform.position, 0.5f);
+        if (arrow_left != null) 
+        {
+            arrow_left.GetComponent<SpriteRenderer>().color = arrowColor;
+            arrow_left.transform.localScale = new Vector3(arrowScale, arrowScale, arrowScale);
+            arrow_left.transform.localRotation = Quaternion.Euler(arrowAngle, 0, 0);
+        }
+
+        if (arrow_right != null)
+        {
+            arrow_right.GetComponent<SpriteRenderer>().color = arrowColor;
+            arrow_right.transform.localScale = new Vector3(arrowScale, arrowScale, arrowScale);
+            arrow_right.transform.localRotation = Quaternion.Euler(arrowAngle, 0, 0);
+        }
+    }
+
+    Vector3 LerpByDistance(Vector3 A, Vector3 B, float x) 
+    {
+        Vector3 P = A + x * (B - A);
+        return P;
+    }
+
+}
