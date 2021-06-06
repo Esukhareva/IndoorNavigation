232d97f69878bffe7226237e5d1c95144eecc59b
DESKTOP-294T7EA\killk <suharevakati@gmail.com>
2021-06-06 20:28:37 +0300

    фильтр по поиску

--- Assets/Scripts/PathDrawer.cs
+++ Assets/Scripts/PathDrawer.cs
@@ -9,41 +9,59 @@ public class PathDrawer : MonoBehaviour
     public GameObject trigger; // trigger to spawn and despawn AR arrows
     public Transform target; //current chosen destination
     public GameObject person; //person indicator
+    public InputField inputField; //person indicator
     public Text text; //information text box
 
     public Dropdown dropdown; //dropdown of destinations
 
     public GameObject destinationLocations;
 
-    private Dictionary<int, Transform> destinationMap;
+    private Dictionary<string, Transform> destinationMap;
     private NavMeshPath path; //current calculated path
     private LineRenderer line; //line renderer to display path
 
     private bool destinationSet; //bool to say if a destination
-
+    private List<string> aListDestination;
+    private List<Dropdown.OptionData> dropdownOptions;
     //create initial path, get linerenderer and fill dropdown.
     void Start()
     {
         path = new NavMeshPath();
         line = transform.GetComponent<LineRenderer>();
-        destinationMap = new Dictionary<int, Transform>();
-        FillDropDown();
+        destinationMap = new Dictionary<string, Transform>();
+        aListDestination = new List<string>();
+        int i = 0;
+        aListDestination.Add("Выберите аудиторию..");
+        foreach (Transform dest in destinationLocations.transform)
+        {
+            destinationMap.Add(dest.name, dest);
+            aListDestination.Add(dest.name);
+            i++;
+        }
+        FillDropDown(aListDestination);
         destinationSet = false;
     }
 
-    private void FillDropDown()
+    public void FilterDropDown(string input) 
     {
-        List<string> aList = new List<string>();
-        aList.Add("Выберите аудиторию..");
-
-        int i = 0;
-        foreach (Transform dest in destinationLocations.transform) 
+        destinationSet = false;
+        List<string> aNewListDestination = new List<string>();
+        aNewListDestination.Add("Выберите аудиторию..");
+        for (int i = 0; i < aListDestination.Count; i++)
         {
-            destinationMap.Add(i, dest);
-            aList.Add(dest.name);
-            i++;
+            if (aListDestination[i].StartsWith(inputField.text))
+            {
+                aNewListDestination.Add(aListDestination[i]);
+            }
         }
-        dropdown.AddOptions(aList);
+        FillDropDown(aNewListDestination);
+    }
+
+    private void FillDropDown(List<string> theListDestination)
+    {
+        dropdown.options.Clear();
+        dropdown.AddOptions(theListDestination);
+        dropdownOptions = dropdown.options;
     }
 
     // Update is called once per frame
@@ -70,19 +88,21 @@ public class PathDrawer : MonoBehaviour
     }
 
     //set current destination and create a trigger for showing AR arrows
-    public void setDestination(int index) 
+    public void setDestination(string aDestination) 
     {
         //if (!destinationMap.TryGetValue(thePlaceName, out target))
         //{
         //    text.text = "Something went wrong :^(";
         //}
-        target = destinationMap[index];
+
+        target = destinationMap[aDestination];
         GameObject.Instantiate(trigger, person.transform.position, person.transform.rotation);
     }
 
     //dropdown listener
     public void DropDownIndexChanged(int index) 
     {
+        string aValue = dropdown.options[dropdown.value].text;
 
         if (index == 0 && !destinationSet)
         {
@@ -95,13 +115,13 @@ public class PathDrawer : MonoBehaviour
             if (destinationSet)
             {
                 RemoveArrowAndCollider();
-                setDestination(index);
+                setDestination(aValue);
             }
             else 
             {
                 dropdown.options.RemoveAt(0);
                 dropdown.SetValueWithoutNotify(index - 1);
-                setDestination(index - 1);
+                setDestination(aValue);
             }
 
             destinationSet = true;
@@ -115,7 +135,7 @@ public class PathDrawer : MonoBehaviour
         target = null;
         line.positionCount = 0;
         dropdown.ClearOptions();
-        FillDropDown();
+        FillDropDown(aListDestination);
         dropdown.SetValueWithoutNotify(0);
         RemoveArrowAndCollider();
         destinationSet = false;
